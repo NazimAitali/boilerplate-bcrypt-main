846eb15a19c9c0818647e79d9f44b4c8fc299390
NazimAitali <nazimaitali88@gmail.com>
2022-08-07 22:30:47 +0100

    hashAndCompare-fix

--- server.js
+++ server.js
@@ -10,17 +10,15 @@ const myPlaintextPassword = "sUperpassw0rd!";
 const someOtherPlaintextPassword = "pass123";
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash('passw0rd!', 13, (err, hash) => {
-    console.log(hash);
-    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-    bcrypt.compare('passw0rd!', hash, (err, res) => {
-      console.log(res); //true
-    });
+bcrypt.hash("passw0rd!", 13, (err, hash) => {
+  console.log(hash);
+  //END_ASYNC
+  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  //START_SYNC
+  bcrypt.compare("passw0rd!", hash, (err, res) => {
+    console.log(res); //true
   });
-//END_ASYNC
-
-//START_SYNC
-
+});
 //END_SYNC
 
 app.listen(process.env.PORT || 3000, () => {});
