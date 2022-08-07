93dc44354b6f00eea95dd96f25b3a5bf6252fb5a
NazimAitali <nazimaitali88@gmail.com>
2022-08-07 23:16:44 +0100

    Synchronously-fix

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //START_SYNC
 var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
 var result = bcrypt.compareSync(myPlaintextPassword, hash);
-
+console.log(result)
 //END_SYNC
 
 app.listen(process.env.PORT || 3000, () => {});
